f9319954b962d6c662145494a89f8746068a4022
Master <86474801+mhmdeve@users.noreply.github.com>
2026-08-09 11:34:17 +0500

    ui: apply modern visual styling to main window

--- APKEasyTool/Program.cs
+++ APKEasyTool/Program.cs
@@ -12,25 +12,19 @@ namespace APKEasyTool
 {
     static class Program
     {
-
         public static readonly string Name = "APK Easy Tool";
-
         public static readonly string Version = "1.61";
-
 
         [DllImport("Shcore.dll")]
         static extern int SetProcessDpiAwareness(int PROCESS_DPI_AWARENESS);
 
-        // According to https://msdn.microsoft.com/en-us/library/windows/desktop/dn280512(v=vs.85).aspx
         private enum DpiAwareness
         {
             None = 0,
             SystemAware = 1,
             PerMonitorAware = 2
         }
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main(String[] arg)
         {
@@ -43,23 +37,21 @@ namespace APKEasyTool
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            //LoadLang
+            // Load language/configuration.
             LoadConfig();
             Lang.LoadStr();
-            //Force Tls12
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
 
-            //Check resource
             if (ResourcesExist())
             {
-                Application.Run(new MainForm());
+                Application.Run(ModernUi.Apply(new MainForm()));
             }
             else
             {
                 MessageBox.Show(Lang.RESOURCE_MISSING_NOTICE, Program.Name, MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-
         }
+
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         private static extern bool SetProcessDPIAware();
 
@@ -69,7 +61,6 @@ namespace APKEasyTool
             {
                 if (File.Exists(Variables.GetPath() + "config.xml"))
                 {
-                    //Vars.GetPath()
                     XmlSerializer xs = new XmlSerializer(typeof(APKEasyTool));
                     FileStream read = new FileStream(Variables.GetPath() + "config.xml", FileMode.Open, FileAccess.Read, FileShare.Read);
                     APKEasyTool info = (APKEasyTool)xs.Deserialize(read);
@@ -82,17 +73,11 @@ namespace APKEasyTool
                 Debug.WriteLine(ex.ToString());
             }
         }
+
         static bool ResourcesExist()
         {
-            //Resources
-            if (Directory.Exists(Variables.RealPath("Apktool")) && Directory.Exists(Variables.RealPath("Resources")))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            return Directory.Exists(Variables.RealPath("Apktool")) &&
+                   Directory.Exists(Variables.RealPath("Resources"));
         }
     }
 }
