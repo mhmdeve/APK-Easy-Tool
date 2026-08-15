73f46040fba3727586871e193d12e835ad825885
Master <86474801+mhmdeve@users.noreply.github.com>
2026-08-15 12:30:12 +0500

    Bump application version to v1.62

--- APKEasyTool/Properties/AssemblyInfo.cs
+++ APKEasyTool/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// associated with this assembly.
 [assembly: AssemblyTitle("APK Easy Tool")]
 [assembly: AssemblyDescription("User friendly GUI APK Tool.")]
 [assembly: AssemblyConfiguration("")]
@@ -16,7 +16,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to expose a type to COM, set this to true.
+// to COM components.  If you need to expose the assembly to COM, set this to true.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 // Major Version, Minor Version, Build Number, Revision
-[assembly: AssemblyVersion("1.6.1.0")]
-[assembly: AssemblyFileVersion("1.6.1.0")]
+[assembly: AssemblyVersion("1.6.2.0")]
+[assembly: AssemblyFileVersion("1.6.2.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
